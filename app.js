e6e97b10a09cadad633c27f96bfa25846353ef82
Ron Dronet <ron@dronetdevdesign.com>
2022-07-14 10:32:18 -0500

    creating dynamic HTML using a JS function

--- app.js
+++ app.js
@@ -1,16 +1,41 @@
-const profileDataArgs = process.argv.slice(2, process.argv.length);
-console.log(profileDataArgs);
+const generatePage = (name, gitHub) => {
+  return `
+  <!DOCTYPE html> 
+  <html lang="en"> 
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Portfolio Demo</title>
+  </head>
 
-const printProfileData = profileDataArray => {
-  // This ...
-  for (let i = 0; i < profileDataArray.length; i++) {
-    console.log(profileDataArray[i]);
-  }
-
-  console.log("====================================");
-
-  // Is the same as this ...
-  profileDataArray.forEach(profileItem => console.log(profileItem));
+  <body>
+    <h1>${name}</h1>
+    <h2><a href="https://github.com/${gitHub}">Github</a></h2>
+  </body>
+  </html>
+  `;
 };
+// console.log(generatePage("Ron", "DronetDevDesign"));
 
-printProfileData(profileDataArgs);
+const profileDataArgs = process.argv.slice(2, process.argv.length);
+const [name, gitHub] = profileDataArgs;
+console.log(name, gitHub);
+console.log(generatePage(name, gitHub));
+
+// const profileDataArgs = process.argv.slice(2, process.argv.length);
+// console.log(profileDataArgs);
+
+// const printProfileData = profileDataArray => {
+//   // This ...
+//   for (let i = 0; i < profileDataArray.length; i++) {
+//     console.log(profileDataArray[i]);
+//   }
+
+//   console.log("====================================");
+
+//   // Is the same as this ...
+//   profileDataArray.forEach(profileItem => console.log(profileItem));
+// };
+
+// printProfileData(profileDataArgs);
